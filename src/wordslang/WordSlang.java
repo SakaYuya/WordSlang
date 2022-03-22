d231ddb013d67d42d3a479cdfa8a677589354edf
SakaYuya <nguyennam20022006@gmail.com>
2022-03-22 21:22:52 +0700

    Connect database

---
+++ src/wordslang/WordSlang.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package wordslang;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import wordslang.queryData;
+
+/**
+ *
+ * @author ITshop
+ */
+public class WordSlang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, FileNotFoundException{
+        // TODO code application logic here
+        queryData test = new queryData();
+    }
+
+    public WordSlang() {
+        
+    };
+}
